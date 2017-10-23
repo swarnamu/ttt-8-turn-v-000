450ed0e7236f25b6c3556fbd9af222853086cabb
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-10-23 12:45:38 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,7 +39,7 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index)
-    puts display_board(board)
+    display_board(board)
   else
     turn(board)
   end
